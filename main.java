3d95fcd8cf5c5364a65b270d6fe05b4f62f7ad31
max-ellis <mjellis@ualberta.ca>
2019-11-23 18:52:43 -0700

    moved everything to helper

--- main.java
+++ main.java
@@ -3,26 +3,25 @@
 public class main {
 
     public static void main(String args[]){
-        mult();
+        helper.mult();
     }
 
-    private static void mult(){
-	helper h = new helper();
-        int x = h.getIntX();
-        int y = h.getIntY();
-        print(x, y);
-    }
-
-
-    private static void print(int a, int b){
-        System.out.println(a*b);
-    }
 
 
 }
 
 
 class helper {
+
+    public static void mult(){
+        int x = getIntX();
+        int y = getIntY();
+        print(x, y);
+    }
+
+    public static void print(int a, int b){
+        System.out.println(a*b);
+    }
 
     public static int getIntX(){
         return 5;
